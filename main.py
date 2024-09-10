464048cbfafaaceac21bce724c3de6251f651c22
DragonsLordJacob <jacob.hallburns02@gmail.com>
2024-09-10 11:06:24 -0400

    Create main.py
    Opens an application GUI

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter
+import customtkinter
+
+# System Settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# Our app frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("Note Book")
+
+# Run app
+app.mainloop()
