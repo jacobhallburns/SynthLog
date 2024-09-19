8204590952211bc58a96c0a7c378e7a5534fa78a
DragonsLordJacob <jacob.hallburns02@gmail.com>
2024-09-19 14:36:08 -0400

    Adding New Files work in progress
    Working on it

--- Notepage_functionality_Jacob_Hall_Burns/open_file.py
+++ Notepage_functionality_Jacob_Hall_Burns/open_file.py
@@ -1,7 +1,6 @@
-import tkinter
-from tkinter import filedialog
 import file_dialog
 
 def open_file_cmd():
    file = file_dialog.file_dialog_cmd()
-   print(file)
+   return file
+   
--- Notepage_functionality_Jacob_Hall_Burns/sample_main.py
+++ Notepage_functionality_Jacob_Hall_Burns/sample_main.py
@@ -1,8 +1,7 @@
-import tkinter
+import tkinter as tk
 import customtkinter
 from CTkMenuBar import *
-import open_file
-
+from open_file import open_file_cmd
 
 # System Settings
 customtkinter.set_appearance_mode("System")
@@ -15,7 +14,7 @@ app.title("SynthLog")
 
 
 
-# tool bar
+# Tool bar
 menu = CTkTitleMenu(app, x_offset=130)
 button_1 = menu.add_cascade("Folder")
 button_2 = menu.add_cascade("File")
@@ -23,18 +22,17 @@ button_2 = menu.add_cascade("File")
 dropdown2 = CustomDropdownMenu(widget=button_2)
 dropdown2.add_option(option="New")
 dropdown2.add_separator()
-dropdown2.add_option(option="Open", command=lambda: open_file.open_file_cmd())
+dropdown2.add_option(option="Open", command=lambda: open_file_cmd())
 dropdown2.add_separator()
 sub_menu1 = dropdown2.add_submenu("Save")
 sub_menu1.add_option(option="Save")
 sub_menu1.add_option(option="Save As...")
 sub_menu1.add_option(option="Save As A Copy...")
-
 dropdown2.add_separator()
-
 sub_menu2 = dropdown2.add_submenu("Export As")
 sub_menu2.add_option(option=".TXT")
 sub_menu2.add_option(option=".PDF")
+
 
 
 # Run app until closed
