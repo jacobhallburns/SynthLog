826d3044bb73a7b2c1353dd3f74cf05cb342bb1d
DragonsLordJacob <jacob.hallburns02@gmail.com>
2024-11-05 16:21:38 -0500

    Reset

--- Notepage_functionality_Jacob_Hall_Burns/file_dialog.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import filedialog
-
-def file_dialog_cmd():
-   root = tk.Tk()
-   root.withdraw()
-   file_path = filedialog.askopenfilename(title="Open File")
-   root.destroy()
-   return file_path
--- Notepage_functionality_Jacob_Hall_Burns/open_file.py
+++
@@ -1,6 +0,0 @@
-import file_dialog
-
-def open_file_cmd():
-   file = file_dialog.file_dialog_cmd()
-   return file
-   
