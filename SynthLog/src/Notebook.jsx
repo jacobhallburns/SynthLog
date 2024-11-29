ce522bc52199e75b2160c4f5b05cc295ee758192
Dvizee <vincenzobarager@gmail.com>
2024-11-29 15:19:40 -0500

    Notebook name display fix

--- SynthLog/src/Notebook.jsx
+++ SynthLog/src/Notebook.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {useLocation} from 'react-router-dom';
 
 const MyNotebook = () => {
+    // these 2 lines get notebook name from boot.jsx
+    const location = useLocation();
+    const notebookName = location.state?.notebookName;
+
     return (
         <div style={{ textAlign: 'center', marginTop: '20px' }}>
-            <h1>My Notebook</h1>
+            <h1>{notebookName}</h1>
         </div>
     );
 };
